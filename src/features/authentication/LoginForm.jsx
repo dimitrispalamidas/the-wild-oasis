4030784703853b832fde6901c3d7ecde821b4189
Dimitris Palamidas <palamidas.dim@gmail.com>
2024-08-28 22:05:37 +0300

    Update loginform

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("testuser@gmail.com");
+  const [password, setPassword] = useState("12345678");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
@@ -27,30 +27,30 @@ function LoginForm() {
 
   return (
     <Form onSubmit={handleSubmit}>
-      <FormRowVertical label="Email address">
+      <FormRowVertical label='Email address'>
         <Input
-          type="email"
-          id="email"
+          type='email'
+          id='email'
           // This makes this form better for password managers
-          autoComplete="username"
+          autoComplete='username'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           disabled={isLoading}
         />
       </FormRowVertical>
 
-      <FormRowVertical label="Password">
+      <FormRowVertical label='Password'>
         <Input
-          type="password"
-          id="password"
-          autoComplete="current-password"
+          type='password'
+          id='password'
+          autoComplete='current-password'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           disabled={isLoading}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large" disabled={isLoading}>
+        <Button size='large' disabled={isLoading}>
           {!isLoading ? "Log in" : <SpinnerMini />}
         </Button>
       </FormRowVertical>
